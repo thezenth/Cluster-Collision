175f939fe2484f013187508502e4aa5cf8c7021d
Noah Wolfe <darthnoah.wolfe@gmail.com>
2016-05-09 10:00:37 -0400

    Update plotter.py

--- plotter.py
+++ plotter.py
@@ -10,9 +10,15 @@ def read_csv(file_name, n):
         if not line.contains("id")
             id_idx = line.index(",")
             m_idx = line.index("kg,")
-            x_idx = line.index("uh") #Manually insert some sort of custom indexer value for x, y, z positions?
+            x_idx = line.index("FIXADSNL") #Manually insert some sort of custom indexer value for x, y, z positions?
             ident = line[:id_idx]
             mass = line[id_idx:m_idx]
-            #etc
-            data[line][0] = ident
+            x = line[m_idx:x_idx]
+            y = line[x_idx:y_idx]
+            z = line[y_idx:z_idx]
+            data[line][0] = ident #Put data into the array
+            data[line][1] = mass
+            data[line][2] = x
+            data[line][3] = y
+            data[line][4] = z
 
