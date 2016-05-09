b533e18250151c3d44d30d3d5c091ecfb56e9444
Noah Wolfe <Noah Wolfe>
2016-05-08 21:20:18 -0400

    Finished basic wrapper

--- wrapper.py
+++ wrapper.py
@@ -1,3 +1,14 @@
 import cluster_collision
 
-print ("test")
+sim_name = raw_input("Simulation Name: ")
+numb = int(raw_input("Number of Stars: ")) #Turn these inputs into integers at very beginning!
+time_step = int(raw_input("Timestep (Myr): "))
+total_time = int(raw_input("Total Runtime: "))
+
+#Run simulation
+cluster_collision.do_simulation(
+            sim_name,
+            numb,
+            time_step,
+            total_time
+        )
