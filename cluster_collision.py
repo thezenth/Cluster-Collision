2511d6faa58c7c856792775c0532fb18d1c57f3a
Noah Wolfe <Noah Wolfe>
2016-05-08 22:57:44 -0400

    Added ability to specify number of stars in each cluster

--- cluster_collision.py
+++ cluster_collision.py
@@ -1,19 +1,36 @@
+# -- BASIC -- #
+import os
+import numpy as np
+
+# -- AMUSE -- #
 from amuse.units import nbody_system
 from amuse.units import units
 from amuse.ic.plummer import new_plummer_sphere
-import numpy as np
 from amuse.datamodel import Particles
 from amuse.community.hermite0.interface import Hermite
 from amuse.ic.salpeter import new_salpeter_mass_distribution
 
-import os
 
-def do_simulation(nme, number, timestep, total_runtime):
+
+def write_csv_log(f_name, particle_set):
+    f1 = open(f_name, "w") #Open file (make new one), and write to it (using "w")
+    f1.write("id,mass,x,y,z\n") #Write column headers of id, mass, and position in x,y,z
+    for i in range(0,len(particle_set)): #for every particle from 0 to length-1
+        part = particle_set[i] #Get specific particle
+        f1.write(
+            str(i) + "," + #using num instead of id for now.
+            str(part.mass) + "," +
+            str(part.position.x) + "," +
+            str(part.position.y) + "," +
+            str(part.position.z) + 
+            "\n"
+        ) #Write id, mass, and position (x,y,z) and include \n to ensure it goes to newline on the next entry
+    f1.close()
+
+def do_simulation(nme, num_sim_1, num_sim_2, timestep, total_runtime):
 
     name = nme #Name of the specific simulation
     outputPath = "output/" + name #Where the output csv files will go
-
-    num = number #number of stars in each cluster
 
     dt = timestep | units.Myr #timestep
     rt = total_runtime | units.Myr #total runtime
@@ -23,14 +40,14 @@ def do_simulation(nme, number, timestep, total_runtime):
 
     #Create 1st plummer sphere
     print ("Populating star clusters")
-    plummer_1 = new_plummer_sphere(num, convert_nbody)
+    plummer_1 = new_plummer_sphere(num_sim_1, convert_nbody)
     plummer_1.x += 10.0 | units.lightyear #Move stars 10 ly to the "right"
-    plummer_1.mass = new_salpeter_mass_distribution(num) #Provide mass for stars based on salpeter mass distribution
+    plummer_1.mass = new_salpeter_mass_distribution(num_sim_1) #Provide mass for stars based on salpeter mass distribution
 
     #Create 2nd plummer sphere
-    plummer_2 = new_plummer_sphere(num, convert_nbody)
+    plummer_2 = new_plummer_sphere(num_sim_2, convert_nbody)
     plummer_2.x -= 10.0 | units.lightyear #Move stars 10 ly to the "left"
-    plummer_2.mass = new_salpeter_mass_distribution(num) #Provide mass for stars based on salpeter mass distribution
+    plummer_2.mass = new_salpeter_mass_distribution(num_sim_2) #Provide mass for stars based on salpeter mass distribution
 
     #Creates a set of particles, "stars", and then adds each plummer sphere to it
     stars = Particles()
@@ -48,36 +65,24 @@ def do_simulation(nme, number, timestep, total_runtime):
     print ("Creating folder at output path")
     os.mkdir(outputPath) #Make folder in output folder
 
-    #f_start_name = outputPath + "/time_0.0 Myr.txt"
-    #f_start = open(f_start_name, "w")
-    #f_start.write("id,mass,x,y,z\n")
+    f_start_name = outputPath + "/time_0.0 Myr.txt" 
+    write_csv_log(f_start_name, hermite_code.particles) #Write first CSV log at time 0.0
 
     print ("Begin simulation...")
     #Now it is simulation time
-    while (t < rt):
+    while (t <= rt):
         print ("Time:" + str(t))
         hermite_code.evolve_model(t) #evolve it up to that time, t
     
         t += dt
 
         print ("    Outputting to CSV")
-        f_name = outputPath + "/time_" + str(t) + ".txt" #File name for csv's is like "outputPath/time_t.csv"
-        f1 = open(f_name, "w") #Open file (make new one), and write to it (using "w")
-        f1.write("id,mass,x,y,z\n") #Write column headers of id, mass, and position in x,y,z
-        for i in range(0,len(hermite_code.particles)): #for every particle from 0 to length-1
-            part = hermite_code.particles[i] #Get specific particle
-            f1.write(
-                str(i) + "," + #using num instead of id for now..
-                str(part.mass) + "," +
-                str(part.position.x) + "," +
-                str(part.position.y) + "," +
-                str(part.position.z) +
-                "\n"
-                ) #Write id, mass, and position (x,y,z) and include \n to ensure it goes to newline on the next entry
-        f1.close()
+        file_name = outputPath + "/time_" + str(t) + ".txt" #File name for csv's is like "outputPath/time_t.csv"
+        write_csv_log(file_name, hermite_code.particles)
 
         #Note: if you want to include SSE or SEBA codes (stellar evolution), make sure to reset the mass _after_ running that code for each step, as the mass of those stars may change due to stellar evolution
         #Ex: hermite_code.particles.mass = stellar_evolution_code.particles.mass
+        #Also: File name should go up to end time, because there is a log for time 0.0, and then time is incremented *before* the file is named
 
     print ("Simulation complete.")
     hermite_code.stop()
--- wrapper.py
+++ wrapper.py
@@ -1,14 +1,16 @@
 import cluster_collision
 
 sim_name = raw_input("Simulation Name: ")
-numb = float(raw_input("Number of Stars: ")) #Turn these inputs into floats at very beginning!
+numb_1 = float(raw_input("Number of Stars (Cluster 1): ")) #Turn these inputs into floats at very beginning!
+numb_2 = float(raw_input("Number of Stars (Cluster 2): "))
 time_step = float(raw_input("Timestep (Myr): "))
 total_time = float(raw_input("Total Runtime: "))
 
 #Run simulation
 cluster_collision.do_simulation(
             sim_name,
-            numb,
+            numb_1,
+            numb_2,
             time_step,
             total_time
         )
