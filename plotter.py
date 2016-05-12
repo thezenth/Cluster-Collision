63ceda8c8b4b9639e183946db864ff8341ff665f
Noah Wolfe <Noah Wolfe>
2016-05-11 21:59:12 -0400

    Streamlined csv --> data structure with star class

--- plotter.py
+++ plotter.py
@@ -1,9 +1,23 @@
 import os
 import numpy as np
 
+# -- MATPLOTLIB -- #
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+
+class star:
+    def __init__(self, identifier, mss, xpos, ypos, zpos):
+        self.ID = identifier
+        self.mass = mss
+        
+        #Position
+        self.x = xpos
+        self.y = ypos
+        self.z = zpos
+
 def read_csv(file_name, n):
     #ALWAYS MAKE SURE THE NUMPY.ZEROS HAS TWO SETS OF BRACKETS
-    data = np.zeros((n, 5)) #Make a multidimensional numpy array of zeros, with n number of arrays, and each array having 5 entries (id, mass, x, y, z)
+    data = [] #Make an empty list for the stars
     print (data)
     
     f = open(file_name, "r")
@@ -20,12 +34,26 @@ def read_csv(file_name, n):
             x = line[m_idx+2:x_idx]
             y = line[x_idx+2:y_idx]
             z = line[y_idx+2:z_idx]
-            data[i][0] = float(ident) #Put data into the array
-            data[i][1] = float(mass) #Line - 1 because the loop skips the first line
-            data[i][2] = float(x)
-            data[i][3] = float(y)
-            data[i][4] = float(z)
+
+            new_star = star(ident, mass, x, y, z)
+            data.append(new_star)
+
+
+            #data[i][0] = float(ident) #Put data into the array
+            #data[i][1] = float(mass) #Line - 1 because the loop skips the first line
+            #data[i][2] = float(x)
+            #data[i][3] = float(y)
+            #data[i][4] = float(z)
             i+=1
+
+
     return data
 
-#print (read_csv(path1, 125))
+#path1 = "time_0.0.txt"
+
+#def to_plot(plot_data):
+#    fig = plt.figure()
+#    ax = fig.add_subplot(111, projection='3d')
+
+
+#print (read_csv(path1, 125)[0].x)
